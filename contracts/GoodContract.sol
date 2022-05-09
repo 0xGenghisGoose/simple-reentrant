7bdb5d17b0ccda8cc9cab6ca2ab28e1fbd298a48
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-05-09 16:32:29 -0500

    contracts both complete

---
+++ contracts/GoodContract.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract GoodContract {
+    mapping(address => uint256) public balances;
+
+    // update `balances` mapping to include the new ETH deposited by msg.sender
+    function addBalance() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0);
+        (bool sent, ) = msg.sender.call{value: balances[msg.sender]}("");
+        require(sent, "Failed to send Ether");
+
+        // this code is unreachable to reentrancy attack
+        balances[msg.sender] = 0;
+    }
+}
